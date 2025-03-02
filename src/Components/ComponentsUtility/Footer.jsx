15a6e69ead7346399e5147d1c64492d41f533739
Shubham Karna <skarna230@gmail.com>
2025-03-02 19:30:48 +0530

    fixed Footer and added icons

--- src/Components/ComponentsUtility/Footer.jsx
+++ src/Components/ComponentsUtility/Footer.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 export default function Footer() {
-  return (
-    <div className=' flex justify-between px-10 py-10 bg-[#171717] border-[#1e1e1e] border-2 border-t-0'>
-        <div className=' flex flex-col '>
-            <p className=' font-Helvetica font-bold text-[24px] text-[#6366f1] '>PrepX</p>
-            <p className=' text-white/50 font-Helvetica py-10 '>
-            Helping students excel in technical interviews <br /> and assessments with comprehensive practice <br /> tests and  personalized analytics
-            </p>
-        </div>
-
-        <div className=' flex flex-col'>
-            <p className='font-Helvetica text-[#6366f1] text-lg text-right  font-medium'>Quicklinks</p>
-
-            <div className=' py-2 '>
-            <p className='text-white/50 text-right font-Helvetica'>Home</p>
-            <p className='text-white/50 text-right  font-Helvetica'>Subjects</p>
-            <p className='text-white/50 text-right  font-Helvetica'>Companeies</p>
+    return (
+        <div className=' flex justify-between px-10 py-10 bg-[#171717] border-[#1e1e1e] border-2 border-t-0'>
+            <div className=' flex flex-col '>
+                <p className=' font-Helvetica font-bold text-[24px] text-[#6366f1] '>PrepX</p>
+                <p className=' text-white/50 font-Helvetica py-10 '>
+                    Helping students excel in technical interviews <br /> and assessments with comprehensive practice <br /> tests and  personalized analytics
+                </p>
             </div>
-            
+
+
+            <div className=' flex flex-col'>
+                <p className='font-Helvetica text-[#6366f1] text-lg text-center  font-medium'>Social Media Links </p>
+
+                <div className=' py-2  flex gap-3 '>
+                    <SocialIcon url='https://www.linkedin.com/in/shubh4mkarna/'  bgColor='[#171717]'/>
+                    <SocialIcon url='https://github.com/SHUBH4M13' />
+                    <SocialIcon url='https://x.com/shubhamkarna13?s=21' />
+
+                </div>
+
+            </div>
+
+
+            <div className=' flex flex-col'>
+                <p className='font-Helvetica text-[#6366f1] text-lg text-right  font-medium'>Quicklinks</p>
+
+                <div className=' py-2 '>
+                    <p className='text-white/50 text-right  cursor-pointer  hover:text-white duration-200 font-Helvetica'>Home</p>
+                    <p className='text-white/50 text-right  cursor-pointer hover:text-white duration-200 font-Helvetica'>Subjects</p>
+                    <p className='text-white/50 text-right  cursor-pointer hover:text-white duration-200  font-Helvetica'>Companes</p>
+                </div>
+
+
+            </div>
+
+
 
         </div>
-
-    </div>
-  )
+    )
 }
--- src/Components/HomePage/MacbookProcess.jsx
+++ src/Components/HomePage/MacbookProcess.jsx
@@ -25,7 +25,7 @@ export default function MacbookProcess () {
                     <p>Q7. Which of the following is a non-preemptive scheduling algorithm?</p>
                 </div>
                 <div className=' flex flex-col gap-4 px-3 py-8 '>
-                    <Option OptionA="A." OptionDesc="FCFS ( First Come First Serve) " />
+                    <Option OptionA="A." OptionDesc="FCFS ( First Come First Serve ) " />
                     <Option OptionA="B." OptionDesc="SJF  ( Shortest Job First ) " />
                     <Option OptionA="C." OptionDesc="RR ( Round Robin ) " />
                     <Option OptionA="D." OptionDesc="Multilevel Queue" />
